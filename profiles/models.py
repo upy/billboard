5b406bca4c2541ff5dc1f2afd6cb55ba673824cb
mehmet caner cakici <mehmet@canercakici.com>
2017-12-17 21:22:23 +0300

    add the representative model.
    update the button class of the password change form.
    update load modules and logout text.
    update body padding.

--- profiles/models.py
+++ profiles/models.py
@@ -3,11 +3,18 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
-class Profile(models.Model):
+class Representative(models.Model):
+    """
+    The Representative class defines the main storage point for the customer
+    representative.
+
+    """
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
                                 on_delete=models.CASCADE)
+    phone = models.CharField(_('Phone'), max_length=30,
+                             help_text=_('ex: +901234567890'))
 
     class Meta:
-        db_table = 'profile'
-        verbose_name = _('Profile')
-        verbose_name_plural = _('Profiles')
+        db_table = 'representative'
+        verbose_name = _('Representative')
+        verbose_name_plural = _('Representatives')
