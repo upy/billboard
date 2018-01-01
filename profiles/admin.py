c418e2e68b59360288a19a97e845d954c1f03588
mehmet caner cakici <mehmet@canercakici.com>
2018-01-01 10:19:38 +0300

    add profiles admin.

---
+++ profiles/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from . import models, forms
+
+
+@admin.register(models.Representative)
+class RepresentativeAdmin(admin.ModelAdmin):
+    list_display = (
+        'username', 'first_name', 'last_name', 'email', 'phone', 'is_active')
+    form = forms.RepresentativeForm
