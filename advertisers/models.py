8ffb6dbbecc8e3373d1a28ca802e436173166c49
mehmet caner cakici <mehmet@canercakici.com>
2018-01-13 22:46:15 +0300

    add doc for Advertiser model

--- advertisers/models.py
+++ advertisers/models.py
@@ -3,6 +3,9 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class Advertiser(models.Model):
+    """
+    The Advertiser class defines the main storage point for advertiser.
+    """
     name = models.CharField(_('Name'), max_length=255,
                             help_text=_('Company name'))
     email = models.EmailField(_('E-mail'), help_text=_('E-mail address'))
